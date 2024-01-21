8afb3c373f17ddfcf211cb87c31f2231c389333b
antonioIannotta <antonio.iannotta.ai26@gmail.com>
2024-01-21 19:13:08 +0100

    feat: features setup

--- app/main_window.py
+++ app/main_window.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QLabel,
+                             QLineEdit, QVBoxLayout, QWidget, QStyle, QHBoxLayout, QPushButton)
 
 
 class MainWindow(QMainWindow):
@@ -8,36 +9,52 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Bin2Dec App")
         self.setGeometry(100, 100, 800, 600)
 
-        label = QLabel("Hello, PyQt!", self)
-        label.setGeometry(10, 10, 200, 30)
+        entry_label = QLabel("Hello, PyQt!", self)
+        label_style = "QLabel { color: blue; font-size: 16px; font-weight: bold; }"
+        entry_label.setStyleSheet(label_style)
+        entry_label.setGeometry(370, 10, 200, 30)
 
         # widgets
 
-        self.label = QLabel("Enter a number", self)
-        self.number_input = QLineEdit(self)
-        self.display_label = QLabel("", self)
+        self.insert_number_label = QLabel("Enter a number", self)
+        self.insert_number_input = QLineEdit(self)
+        self.display_result_label = QLabel("", self)
+        self.update_push_button = QPushButton("Result", self)
 
-        #layout
+        # layout
 
-        layout = QVBoxLayout()
-        layout.addWidget(self.label)
-        layout.addWidget(self.number_input)
-        layout.addWidget(self.display_label)
+        self.vertical_layout = QVBoxLayout()
+        self.vertical_layout.setSpacing(1)
+        self.vertical_layout.addWidget(self.insert_number_label)
+        self.vertical_layout.addWidget(self.insert_number_input)
+        self.vertical_layout.addWidget(self.display_result_label)
+        self.vertical_layout.addWidget(self.update_push_button)
 
         container = QWidget()
-        container.setLayout(layout)
+        container.setLayout(self.vertical_layout)
 
         self.setCentralWidget(container)
 
-        self.number_input.textChanged.connect(self.update_display)
+        self.update_push_button.clicked.connect(self.update_display)
 
     def update_display(self):
-        entered_text = self.number_input.text()
-        if "c" in entered_text:
-            self.display_label.setText("Error!")
-            self.number_input.setReadOnly(True)
+        entered_text = self.insert_number_input.text()
+
+        if not self.is_binary_string_valid():
+            self.display_result_label.setText("Error!")
+            self.display_result_label.setStyleSheet("QLabel { color: red; }")
+            self.insert_number_input.setReadOnly(True)
             return
+        else:
+            decimal_number = int(entered_text, 2)
+            self.display_result_label.setText(f"{decimal_number}")
 
-        self.display_label.setText(f"{entered_text}")
-
-
+    def is_binary_string_valid(self):
+        binary_string = self.insert_number_input.text()
+        binary_string.replace("0", "")
+        if binary_string == "":
+            return True
+        elif binary_string.replace("1", "") == "":
+            return True
+        else:
+            return False
